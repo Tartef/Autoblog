c624e514cc7d8501617c6919c921ba4075c6b1e1
Tartef <florian.contie@etu.unilim.fr>
2023-10-13 13:43:16 +0200

    Début des posts

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource("posts", PostController::class);
-
 require __DIR__.'/auth.php';
